661269cad91a53e19eaa0b938add88a04765d1f1
searabbit <46087838+searabbitx@users.noreply.github.com>
2024-09-07 18:14:39 +0200

    Run vhost probes in parallel

--- vhosts.sh
+++ vhosts.sh
@@ -7,12 +7,19 @@ function usage() {
   echo "  -d, --domain       root domain"
   echo "  -w, --wordlist     wordlist file with subdomains"
   echo "  -f, --filter-text  text present in response of invalid subdomains"
+  echo "  -t, --threads      number of concurrent processes (default: 10)"
+  echo "                     (this is used as xargs -P option)"
+  echo "                     set to '0' to tell xargs to run as many processes as possible at a time"
+  echo "  -h, --help         print this help message and exit"
   echo ""
-  echo "EXAMPLES:"
-  echo "  ./vhosts.sh -w ./subdomains.txt -d example.com -f 'Example Domain' http://example.com"
+  echo "EXAMPLE:"
+  echo "  ./vhosts.sh -t 5 -w ./subdomains.txt \\"
+  echo "      -d example.com -f 'Example Domain' \\"
+  echo "      http://example.com"
 }
 
 POSITIONAL_ARGS=()
+THREADS=10
 
 while [[ $# -gt 0 ]]; do
   case $1 in
@@ -30,6 +37,15 @@ while [[ $# -gt 0 ]]; do
       FILTERTEXT="$2"
       shift
       shift
+      ;;
+    -t|--threads)
+      THREADS="$2"
+      shift
+      shift
+      ;;
+    -h|--help)
+      usage
+      exit 0
       ;;
     -*|--*)
       echo "Unknown option $1"
@@ -60,22 +76,26 @@ fi
 
 HOST=${POSITIONAL_ARGS[0]}
 
+# logging helper
+function info() {
+  echo "[+] $@"
+}
+export -f info
+
+# the function that tests a single subdomain
 function test_sub() {
   if curl -H "Host: $1.$DOMAIN" $HOST 2>/dev/null | grep "$FILTERTEXT" >/dev/null; then
     :;
   else
-     echo "Found $1.$DOMAIN" 
+     info "Found $1.$DOMAIN" 
   fi
 }
-
-function info() {
-  echo "[+] $@"
-}
+export -f test_sub
 
 # test connection
-if curl -H "Host: $DOMAIN" $HOST 2>/dev/null 1>/dev/null; then
+if curl --parallel -H "Host: $DOMAIN" $HOST 2>/dev/null 1>/dev/null; then
   info "Successfully connected to $DOMAIN at $HOST"
-  info "Bruteforcing..."
+  info "Bruteforcing $DOMAIN with wordlist $WORDLIST and $THREADS threads..."
 else
   echo "Error! Could not connect to $HOST"
   echo ""
@@ -85,7 +105,4 @@ else
   exit 1
 fi
 
-# brute
-while read sub; do
-  test_sub $sub
-done < $WORDLIST
+cat $WORDLIST | DOMAIN=$DOMAIN HOST=$HOST FILTERTEXT=$FILTERTEXT xargs -n 1 -P $THREADS -I {} bash -c 'test_sub "$@"' _ {}
